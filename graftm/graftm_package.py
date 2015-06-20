dc5c555c47d589c3a364eb73bd0212a5406f9f9c
Ben Woodcroft <donttrustben@gmail.com>
2015-06-20 16:15:38 +1000

    add test and slightly more doco for GraftMPackage

--- graftm/graftm_package.py
+++ graftm/graftm_package.py
@@ -1,31 +1,29 @@
 import json
 import os
 
-class InsufficientGraftMPaackageException(Exception): pass
+class InsufficientGraftMPackageException(Exception): pass
 
 class GraftMPackage:
-    '''Package to represent a graftm package. To start using a package, run
-    pkg = GraftMPackage.acquire, and then retrieve values with e.g. 
-    pkg[GraftMPackage.VERSION_KEY]'''
+    '''Package to represent a GraftM package. To start using a package, run
+    
+    pkg = GraftMPackage.acquire('/path/to/a.gpkg')
+    
+    and then retrieve values with e.g.
      
+    pkg.alignment_hmm_path()
+      #=> '/path/to/a.gpkg/the.hmm'
+    '''
+    
+    _CONTENTS_FILE_NAME = 'CONTENTS.json'
+    
+    # The key names are unlikely to change across package format versions,
+    # so store them here in the superclass
     DIAMOND_DATABASE_KEY = 'diamond_database'
     VERSION_KEY = 'version'
     ALIGNMENT_HMM_KEY = 'aln_hmm'
     SEARCH_HMM_KEY = "search_hmm"
     REFERENCE_PACKAGE_KEY = "rfpkg"
     HMM_TRUSTED_CUTOFF_KEY = "TC"
-    
-    _CONTENTS_FILE_NAME = 'CONTENTS.json'
-    _CURRENT_VERSION = 2
-    
-    _REQUIRED_KEYS = {'2': [
-                     DIAMOND_DATABASE_KEY,
-                     VERSION_KEY,
-                     ALIGNMENT_HMM_KEY,
-                     SEARCH_HMM_KEY,
-                     REFERENCE_PACKAGE_KEY,
-                     HMM_TRUSTED_CUTOFF_KEY
-                     ]}
     
     @staticmethod
     def acquire(graftm_package_path):
@@ -36,7 +34,7 @@ class GraftMPackage:
         graftm_output_path: str
             path to base directory of graftm
         '''
-        pkg = GraftMPackage()
+        pkg = GraftMPackageVersion2()
         
         pkg._base_directory = graftm_package_path
         pkg._contents_hash = json.load(open(os.path.join(graftm_package_path,
@@ -44,26 +42,64 @@ class GraftMPackage:
                                              ))
         
         # check we are at current version otherwise choke
-        pkg.check_required_keys(pkg._CURRENT_VERSION)
+        pkg.check_universal_keys(2)
+        pkg.check_required_keys(GraftMPackageVersion2._REQUIRED_KEYS)
         return pkg
-        
-        
-    def check_required_keys(self, version=GraftMPackage.CURRENT_VERSION):
-        '''raise InsufficientGraftMPaackageException if this package does not
-        conform to the standard of the given package'''
-        #check version key
+    
+    def check_universal_keys(self, version):
         h = self._contents_hash
-        v = h[self._VERSION_KEY]
-        if not h[self._VERSION_KEY]:
-            raise InsufficientGraftMPaackageException("No version information in graftm package")
+        try:
+            v = h[GraftMPackage.VERSION_KEY]
+        except KeyError:
+            raise InsufficientGraftMPackageException("No version information in graftm package")
         if v != version:
-            raise InsufficientGraftMPaackageException("Bad version: %s" % v)
+            raise InsufficientGraftMPackageException("Bad version: %s" % v)
         
-        for key in self._REQUIRED_KEYS[str(version)]:
+    def check_required_keys(self, required_keys):
+        '''raise InsufficientGraftMPackageException if this package does not
+        conform to the standard of the given package'''  
+        h = self._contents_hash    
+        for key in required_keys:
             if key not in h:
-                raise InsufficientGraftMPaackageException("package missing key %s" % key)
+                raise InsufficientGraftMPackageException("package missing key %s" % key)
             
     def __getitem__(self, key):
         '''Return the value of the given key from the contents file'''
         return self._contents_hash[key]
+    
+class GraftMPackageVersion2(GraftMPackage):
+    version = 2
+    
+    _REQUIRED_KEYS = [
+                     GraftMPackage.DIAMOND_DATABASE_KEY,
+                     GraftMPackage.VERSION_KEY,
+                     GraftMPackage.ALIGNMENT_HMM_KEY,
+                     GraftMPackage.SEARCH_HMM_KEY,
+                     GraftMPackage.REFERENCE_PACKAGE_KEY,
+                     GraftMPackage.HMM_TRUSTED_CUTOFF_KEY
+                     ]
+    
+    def diamond_database_path(self):
+        return os.path.join(self._base_directory, 
+                            self._contents_hash[GraftMPackage.DIAMOND_DATABASE_KEY])
+    
+    def search_hmm_paths(self):
+        return [os.path.join(self._base_directory, x) for x in
+                self._contents_hash[GraftMPackage.DIAMOND_DATABASE_KEY]]
+        
+    def alignment_hmm_path(self):
+        return os.path.join(self._base_directory, 
+                            self._contents_hash[GraftMPackage.ALIGNMENT_HMM_KEY])
+        
+    def reference_package_path(self):
+        return os.path.join(self._base_directory, 
+                            self._contents_hash[GraftMPackage.REFERENCE_PACKAGE_KEY])
+        
+    def use_hmm_trusted_cutoff(self):
+        return self._contents_hash[GraftMPackage.HMM_TRUSTED_CUTOFF_KEY]
+        
+        
+    
+    
+
             

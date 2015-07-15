86ade166c7dc3a5fc23498789319538e13d14beb
Joel <joel.boyd@uqconnect.edu.au>
2015-07-15 11:16:19 +1000

    merge pre fail diamond

---
+++ graftm/timeit.py
@@ -0,0 +1,14 @@
+import time
+
+class Timer:
+    
+    def __init__(self): pass
+
+    def timeit(self, method):
+        def timed(*args, **kw):
+            ts = time.time()
+            result = method(*args, **kw)
+            te = time.time()
+            return round(te-ts, 2), result
+    
+        return timed

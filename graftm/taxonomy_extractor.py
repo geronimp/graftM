7fa2d6334def9cfb497ee1bad892d24fefe50fff
Ben J Woodcroft <donttrustben@gmail.com>
2015-12-02 10:38:15 +1000

    create: don't read bootstraps as taxonomy in annotated trees

--- graftm/taxonomy_extractor.py
+++ graftm/taxonomy_extractor.py
@@ -1,3 +1,5 @@
+import re
+
 class TaxonomyExtractor:
     def taxonomy_from_annotated_tree(self, tree_node):
         '''Given an annotated tree, return the taxonomy of each tip
@@ -14,8 +16,9 @@ class TaxonomyExtractor:
             tax = []
             n = tip.parent
             while n:
-                if n.name and n.parent:
-                    tax = [n.name]+tax
+                node_taxonomy = self.taxonomy_from_node_name(n.name)
+                if node_taxonomy and n.parent:
+                    tax = [node_taxonomy]+tax
                 n = n.parent
             tip_name = tip.name.replace(' ','_')
             if tip_name in tip_to_taxonomy:
@@ -25,3 +28,39 @@ class TaxonomyExtractor:
             else:
                 tip_to_taxonomy[tip_name] = [t.strip() for t in '; '.join(tax).split(';')]
         return tip_to_taxonomy
+    
+    def taxonomy_from_node_name(self, node_name):
+        '''return the taxonomy incorporated at a particular node, or None
+        if it does not encode any taxonomy
+        
+        Parameters
+        ----------
+        node_name: str
+            a TreeNode.name
+            
+        Returns
+        -------
+        Taxonomy as a string, or None if there is no taxonomy
+        '''
+        
+        def isFloat(s):
+            try:
+                float(s)
+                return True
+            except ValueError:
+                return False
+        
+        if node_name is None:
+            return None
+        elif isFloat(node_name):
+            # no name, just a bootstrap
+            return None
+        else:
+            bootstrap_regex = re.compile(r'[\d\.]+:(.*)')
+            reg = bootstrap_regex.match(node_name)
+            if reg:
+                # bootstrap in name
+                return reg.groups(0)[0]
+            else:
+                # bootstrap not in name
+                return node_name

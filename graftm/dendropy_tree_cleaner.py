2445ab33462919ed5e457db8b2f3703a955decf9
Ben Woodcroft <donttrustben@gmail.com>
2016-08-02 21:53:09 +1000

    graftm: Add 'DendropyTreeCleaner'.

---
+++ graftm/dendropy_tree_cleaner.py
@@ -0,0 +1,21 @@
+from dendropy import Tree
+import copy
+
+class DendropyTreeCleaner:
+    def write_fasttree_newick(self, tree, output_file_io):
+        '''Write the given dendropy tree to the open output file I/O, ensuring that is
+        acceptable for FastTree by e.g. removing quoted taxon names.
+
+        Params
+        ------
+        tree: dendropy.Tree
+            tree to be written
+        output_file_io: stream
+            stream to write the newick formatted tree to
+
+        '''
+        newick = tree.as_string(schema='newick',
+                                suppress_internal_node_labels=True,
+                                suppress_internal_taxon_labels=True,
+                                preserve_spaces=False) # Labels must not be quoted.
+        output_file_io.write(newick.replace("'",'').replace(' ','_'))
